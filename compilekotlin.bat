eb52cd42ba2c316a970c57031667d43b64e08c29
Eric Tsang <surplus.et@gmail.com>
2016-08-07 03:57:13 -0700

    initial commit
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ compilekotlin.bat
@@ -0,0 +1,11 @@
+@echo off
+echo.
+echo compiling kotlin code...
+call kotlinc kotlin\hi\Hello.kt -Xallow-kotlin-package -include-runtime -d KotlinHello.jar
+echo DONE
+echo.
+echo running kotlin code...
+call java -jar KotlinHello.jar
+echo DONE
+echo.
+pause
---
+++ kotlin/hi/Hello.kt
@@ -0,0 +1,6 @@
+package kotlin.hi
+
+fun main(args:Array<String>)
+{
+    println("Hello world!")
+}
